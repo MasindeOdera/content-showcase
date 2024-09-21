7d5063be4bef62788f7fb4b06b8d001d586432af
Masinde Odera <masindeodera@gmail.com>
2024-09-21 21:24:59 +0200

    Adjusted Container path

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PublicationView from './views/PublicationView';
 import PublicationDetail from './components/PublicationDetail';
-import Container from './components/styles/container/container';
+import Container from './components/styles/container/Container';
 
 
 const App: React.FC = () => {
