9d7542084395ea79ca31ffa5273d9eddb58ebb7d
Masinde Odera <masindeodera@gmail.com>
2024-10-15 20:28:12 +0200

    Cleanup GitHub Pages setup and migrate to Vercel

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,10 +3,8 @@ import axios from 'axios';
 export async function getBearerToken() {
   const response = await axios.post('https://api.foleon.com/oauth', {
     grant_type: 'client_credentials',
-    client_id:  'b4xfRRj0xL',
-    client_secret: 'cb9c3aa77a15392f3bb4455d0194b07dc2953694485ed5ba844743323578ffeb',
+    client_id:  import.meta.env.VITE_CLIENT_ID,
+    client_secret: import.meta.env.VITE_CLIENT_SECRET,
   });
   return response.data.access_token;
 };
-
-// would have used import.meta.env.VITE_CLIENT_ID & import.meta.env.VITE_CLIENT_SECRET for security
